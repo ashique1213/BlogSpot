899628bfabc1c6308dd109bc3656533848260d4e
ashique1213 <muhammedashique1213@gmail.com>
2026-06-30 14:30:59 +0530

    chore: remove unused django templates, static files and configurations

--- blogspot/blogspot/settings.py
+++ blogspot/blogspot/settings.py
@@ -102,7 +102,7 @@ SIMPLE_JWT = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -179,7 +179,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
